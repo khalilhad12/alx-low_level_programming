50478825bc814167acee383622e3020072f36717
khalilhad12 <khalilhadrouji12@gmail.com>
2023-03-16 14:47:00 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - this is the main function
  *
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
