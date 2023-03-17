e965a5e33a52f0937049e1d5223b1c3e6199832e
khalilhad12 <khalilhadrouji12@gmail.com>
2023-03-16 17:47:16 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - A с program that prints a line to the standard error
+*Description: 'the program's description'
+*Return 1 (Success)
+*/
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19\n", stdout);
+return (1);
+}
